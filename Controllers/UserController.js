d5887d40a75e037a278b9eec9d16f758320de9e8
Shashank Hegde <shashank.hegde@neutrinos.co>
2021-04-12 12:54:57 +0530

    added user list api

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -3,28 +3,71 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt');
 const User = mongoose.model('Users');
 
-exports.register = function (req, res) {
+exports.signup = async function (req, res) {
     const newUser = new User(req.body);
     newUser.password = bcrypt.hashSync(req.body.password, 10);
-    newUser.save(function (err, user) {
-        if (err) {
-            return res.status(400).send({
-                message: err
-            });
-        } else {
-            res.status(200).send('User Created Succesfully!');
+    try {
+        const existingUser = await User.findOne({ email_id: email_id });
+        if (existingUser)
+            return res.status(409).send("User id already exists...");
+        const resp = await newUser.save();
+        return res.status(200).send('User Created Succesfully!');
+
+    } catch (e) {
+        return res.status(400).send('Bad Request..');
+    }
+
+}
+
+exports.signin = async function (req, res) {
+    const { emailAddress, password } = req.body;
+    try {
+        const user = await User.findOne({ email_id: emailAddress });
+        if (!user)
+            return res.status(404).send('User does not exists!');
+        const passmatch = bcrypt.compareSync(password, user.password);
+        if (!passmatch) {
+            return res.status(401).send('Invalid Credentials..');
         }
-    });
+        const accessToken = jwt.sign({ email_id: emailAddress }, process.env.SECRET_KEY, {
+            expiresIn: 86400 // 24 hours
+        });
+        return res.status(200).json({
+            emailAddress,
+            accessToken
+        });
+
+    } catch (e) {
+        return res.status(400).send('Bad Request..');
+    }
 }
 
-exports.deleteUser = function (req, res) {
+exports.getUsers = async function (req, res) {
+    const { page, limit } = req.query;
+    const skip = (page - 1) * limit;
+    try {
+        const response = await User.aggregate([
+            { $match: {} },
+            { $sort: { created_at: -1 } },
+            { $skip: parseInt(skip) },
+            { $limit: parseInt(limit) },
+            { $project: { "email_id": 1, "created_at": 1 } }]);
+        return res.status(201).send(response);
+    } catch (e) {
+        console.log(e)
+        return res.status(400).send('Bad Request..')
+    }
+}
+
+exports.deleteUser = async function (req, res) {
     const { email } = req.query;
-    User.findOneAndDelete({
-        email_id: email
-    }, (err, docs) => {
-        if (err)
-            console.log('Error in deleting user', err);
-        return res.status(200).json('User has been deleted');
-    })
+    try {
+        const resp = await User.findOneAndDelete({ email_id: email });
+        return res.status(200).send('User id has been removed..');
+
+    } catch (e) {
+        return res.status(400).send('Bad Request..');
+    }
 
 }
+
--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -2,7 +2,11 @@ const UserHandler = require('../Controllers/UserController');
 const SessionHandler = require('../Middlewares/index');
 module.exports = (app) => {
     app.route('/api/user/create')
-        .post(UserHandler.register);
+        .post(UserHandler.signup);
+    app.route('/api/user/signin')
+        .post(UserHandler.signin);
+    app.route('/api/users')
+        .get(UserHandler.getUsers);
     app.route('/api/user/delete')
         .post(UserHandler.deleteUser);
 }
